934346e186fbfc27643aae6b7dbad6e3293c0d1b
dora.xyz <dora.xyz@icloud.com>
2023-05-05 18:55:12 +0900

    コメントを追加

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+// Reactのルートを作成する
 const root = ReactDOM.createRoot(
+  // ルート要素として使用するHTML要素を取得する
   document.getElementById('root') as HTMLElement
 );
+
+// ルートをレンダリングする
 root.render(
+  // ReactのStrictModeコンポーネントを使用して、Appコンポーネントをレンダリングする
   <React.StrictMode>
     <App />
   </React.StrictMode>
